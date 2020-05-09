7dac3b80ac6962f7abc9b8ffdf6b228bdaf25d8f
Molly Cooper <mollycooper64@gmail.com>
2020-05-09 19:32:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,11 +50,11 @@ user_input = gets
 index = input_to_index(user_input)
 
 if valid_move?(board, index)
-  puts display_board(board)
+  move(board, index, character)
 
 else until valid_move?(board, index) == true
   puts "Please enter 1-9:"
-  
+
 end
 end
 
